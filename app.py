b5903d3dd57ced408772822ac3a4d87dfc9c520c
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2023-07-03 14:58:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import *
+import pickle
+import os
+import re
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('Doctors Annual Salary Prediction.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='The annual salary is {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
